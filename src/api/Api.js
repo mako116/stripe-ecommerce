cc95301a97ec050e0c0ce9846ef257c5e1b4c582
mako <nwakwueemmanuel1@gmail.com>
2024-05-08 20:41:34 +0100

    new

--- src/api/Api.js
+++ src/api/Api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 
 export async function productsData(){
-    const products = await axios.get("https://fakestoreapiserver.reactbd.com/products")
+    const products = await axios.get("https://fakestoreapi.com/products")
     return  products;
 }
